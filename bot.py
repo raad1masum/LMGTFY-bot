25b577b13cbf3e0ef5dae3ab55ea453063c28495
raad1masum <piraadwan@gmail.com>
2020-07-29 13:20:31 -0700

    remove y from command prefix so that it looks better

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 TOKEN = ""
 
-client = commands.Bot(command_prefix = "lmgtfy")
+client = commands.Bot(command_prefix = "lmgtf")
 
 @client.event
 async def on_ready():
