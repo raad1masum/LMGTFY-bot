0aafbe7bf9616c36199ac06f729802c11e266477
raad1masum <piraadwan@gmail.com>
2020-07-29 13:00:43 -0700

    generate token and run

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import discord
 from discord.ext import commands
 
+TOKEN = ""
+
 client = discord.Client(command_prefix = "lmgtfy")
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
+    
+client.run(TOKEN)
